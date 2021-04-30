3cc29acaf7729f8a25f365af7647adbfda8946d5
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2021-04-30 08:59:48 +0200

    feat: add website model

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,8 +1,21 @@
 class Website < ApplicationRecord
   belongs_to :user, counter_cache: true
+  before_validation :generate_site_id, on: :create
 
   VALID_URL_ = %r{\A(http|https)://[a-z0-9]+([\-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?/.*)?$\z}ix
 
   validates :site_original_link, format: { with: VALID_URL_ }
+  private
+
+  def generate_site_id
+    generate_token(:uid)
+  end
+
+  def generate_token(column)
+    loop do
+      self[column] = SecureRandom.urlsafe_base64[0..6]
+      break unless self.class.exists?(column => self[column])
+    end
+  end
 
 end
